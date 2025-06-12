817d0456ef3d0c50482d9d455d931c067ae600db
Jonathan Roncancio <jonathan.roncancio@clarity.ai>
2025-06-12 17:06:10 +0200

    Test azure auth with OIDC

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -11,7 +11,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "staucmtfmterraformstate"
+    storage_account_name = "statfmmbeterraformstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   } 
