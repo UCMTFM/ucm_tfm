c836f8b39947af6bcea281f5bd667cddd6be509e
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-07-06 14:40:17 +0200

    Solving errors in terraform apply (KV)

--- infrastructure/modules/key_vault/main.tf
+++ infrastructure/modules/key_vault/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_key_vault_access_policy" "terraform" {
   object_id    = data.azurerm_client_config.current.object_id
 
   secret_permissions = [
-    "get", "set", "list"
+    "Get", "List", "Set"
   ]
 }
 
