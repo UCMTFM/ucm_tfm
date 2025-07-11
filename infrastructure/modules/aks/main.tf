1a792a6f6da31c4622bc0e46ab87e7160f132121
Jonathan Roncancio <47423499+jonathan5p@users.noreply.github.com>
2025-07-11 17:47:34 +0200

    Feat: Adding AKS cluster module (#34)
    * Adding AKS cluster module
    
    * Updating azurerm valid versions

---
+++ infrastructure/modules/aks/main.tf
@@ -0,0 +1,11 @@
+module "aks" {
+  source  = "Azure/aks/azurerm"
+  version = "10.1.1"
+
+  cluster_name        = "aks${var.prefix}${var.name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+  sku_tier            = var.sku_tier
+  prefix              = var.prefix
+}
