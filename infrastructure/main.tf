2c6f651344e1a20365ab4b5ed11dc8dd003c3ed1
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-07-12 13:43:48 +0200

    Adding external locations in unity catalog

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -104,6 +104,18 @@ module "databricks_access_connector" {
   storage_account_id  = module.lakehouse_storage.id
 }
 
+# Unity Catalog
+
+module "unity_catalog" {
+  source                         = "./modules/unity_catalog"
+  databricks_host                = module.databricks_workspace.workspace_url
+  workspace_resource_id          = module.databricks_workspace.id
+  prefix                         = var.project
+  access_connector_id            = module.databricks_access_connector.id
+  lakehouse_external_layers      = ["bronze", "silver", "gold"]
+  lakehouse_storage_account_name = module.lakehouse_storage.account_name
+}
+
 # Azure k8s Cluster
 
 module "aks" {
