80807f5c59d4e4c5a3266475f46080d47642084e
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-07-12 20:55:13 +0200

    Solving auth issues for credentials creation

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -28,10 +28,19 @@ data "azuread_user" "members" {
   user_principal_name = each.key
 }
 
+data "azuread_service_principal" "github_app" {
+  client_id = var.azure_client_id
+}
+
 resource "azuread_group_member" "members" {
   for_each         = data.azuread_user.members
   group_object_id  = azuread_group.admins.object_id
   member_object_id = each.value.object_id
+}
+
+resource "azuread_group_member" "sp_member" {
+  group_object_id  = azuread_group.admins.object_id
+  member_object_id = data.azuread_service_principal.github_app.object_id
 }
 
 resource "azurerm_role_assignment" "aad_group_rg_contributor" {
