a20ad42c4438a17c1b9d82e78946e4961124f9fa
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-07-15 10:19:01 +0200

    Choosing other cluster

--- infrastructure/modules/databricks_clusters/main.tf
+++ infrastructure/modules/databricks_clusters/main.tf
@@ -11,10 +11,20 @@ provider "databricks" {
   azure_workspace_resource_id = var.databricks_workspace_id
 }
 
-resource "databricks_cluster" "shared_cheapest" {
+data "databricks_cluster_policy" "shared_compute" {
+  name = "Shared Compute"
+}
+
+resource "databricks_cluster" "shared_compute_cluster" {
   cluster_name            = "cluster-${var.prefix}"
   spark_version           = var.spark_version
   node_type_id            = var.node_type_id
+  policy_id               = data.databricks_cluster_policy.shared_compute.id
+
   autotermination_minutes = var.idle_minutes
   num_workers             = var.num_workers
+
+  enable_photon           = false
+
+  data_security_mode      = "USER_ISOLATION"
 }
