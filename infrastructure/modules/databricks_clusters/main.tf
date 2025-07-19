d73e0090e7c1c9d1030358cd44f1e18a2814cb18
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-07-19 12:44:23 +0200

    Re-deploying compute cluster

--- infrastructure/modules/databricks_clusters/main.tf
+++ infrastructure/modules/databricks_clusters/main.tf
@@ -11,12 +11,16 @@ provider "databricks" {
   azure_workspace_resource_id = var.databricks_workspace_id
 }
 
+locals {
+  node_type_hash = substr(md5(var.node_type_id), 0, 6)
+}
+
 data "databricks_cluster_policy" "shared_compute" {
   name = "Shared Compute"
 }
 
 resource "databricks_cluster" "shared_compute_cluster" {
-  cluster_name            = "cluster-${var.prefix}"
+  cluster_name            = "cluster-${var.prefix}-${local.node_type_hash}"
   spark_version           = var.spark_version
   node_type_id            = var.node_type_id
   policy_id               = data.databricks_cluster_policy.shared_compute.id
