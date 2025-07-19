19f8fccd59b9301647ecf37e17a3e0b02ee269da
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-07-19 22:53:58 +0200

    Making cluster Single Node

--- infrastructure/modules/databricks_clusters/main.tf
+++ infrastructure/modules/databricks_clusters/main.tf
@@ -11,10 +11,15 @@ provider "databricks" {
   azure_workspace_resource_id = var.databricks_workspace_id
 }
 
-resource "databricks_cluster" "single_node_cluster" {
+data "databricks_cluster_policy" "unrestricted" {
+  name = "Unrestricted"
+}
+
+resource "databricks_cluster" "shared_compute_cluster" {
   cluster_name            = "cluster-${var.prefix}"
   spark_version           = var.spark_version
   node_type_id            = var.node_type_id
+  policy_id               = data.databricks_cluster_policy.unrestricted.id
   autotermination_minutes = var.idle_minutes
   num_workers             = var.num_workers
 
