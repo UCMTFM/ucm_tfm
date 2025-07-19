460e2eefe517e792937fef11caae57ff761885d0
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-07-20 00:27:58 +0200

    Making cluster Single Node

--- infrastructure/modules/databricks_clusters/main.tf
+++ infrastructure/modules/databricks_clusters/main.tf
@@ -11,22 +11,37 @@ provider "databricks" {
   azure_workspace_resource_id = var.databricks_workspace_id
 }
 
+resource "databricks_cluster_policy" "single_node_policy" {
+  name = "Single Node Policy"
+
+  definition = jsonencode({
+    "num_workers" : {
+      "type" : "fixed",
+      "value" : "0"
+    },
+    "spark_conf.spark.databricks.cluster.profile" : {
+      "type" : "fixed",
+      "value" : "singleNode"
+    },
+    "spark_conf.spark.master" : {
+      "type" : "fixed",
+      "value" : "local[*]"
+    },
+    "data_security_mode" : {
+      "type" : "fixed",
+      "value" : "SINGLE_USER"
+    },
+    "single_user_name" : {
+      "type" : "fixed",
+      "value" : var.databricks_cluster_user
+    }
+  })
+}
+
 resource "databricks_cluster" "shared_compute_cluster" {
-  cluster_name            = "cluster-${var.prefix}"
-  spark_version           = var.spark_version
-  node_type_id            = var.node_type_id
+  cluster_name  = "cluster-${var.prefix}"
+  policy_id     = databricks_cluster_policy.single_node_policy.id
+  spark_version = var.spark_version
+  node_type_id  = var.node_type_id
   autotermination_minutes = var.idle_minutes
-  # data_security_mode      = "SINGLE_USER"
-  num_workers             = var.num_workers
-  # single_user_name        = var.databricks_cluster_user
-
-  spark_conf = {
-    "spark.databricks.cluster.profile" = "singleNode"
-    "spark.master"                     = "local[*]"
-  }
-
-  lifecycle {
-    create_before_destroy = false
-    prevent_destroy       = false
-  }
 }
