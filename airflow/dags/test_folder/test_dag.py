8240b0bc95a09635e07c975b15699dd2c07c7305
jonathan5p <jonathanpinzon05@gmail.com>
2025-07-19 17:36:47 +0200

    Changing test_dag

--- airflow/dags/test_folder/test_dag.py
+++ airflow/dags/test_folder/test_dag.py
@@ -1,8 +1,19 @@
 from datetime import datetime
+from pprint import pprint
 
 from airflow.models.dag import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.decorators import task
+
+
+@task(task_id="print_the_context")
+def print_context(ds=None, **kwargs):
+    """Print the Airflow context and ds variable from the context."""
+    pprint(kwargs)
+    print(ds)
+    return "Whatever you return gets printed in the logs"
+
 
 with DAG(
     dag_id="airflow_installation_test_inside_folder",
@@ -14,18 +25,18 @@ with DAG(
     start_task = EmptyOperator(task_id="start")
 
     # Task to print a simple message
-    print_message = BashOperator(
-        task_id="print_message",
-        bash_command="echo 'Airflow installation test successful!'",
-    )
-
-    # Task to sleep for a few seconds (simulating some work)
-    sleep_task = BashOperator(
-        task_id="sleep_for_5_seconds",
-        bash_command="sleep 5",
-    )
+    # print_message = BashOperator(
+    #     task_id="print_message",
+    #     bash_command="echo 'Airflow installation test successful!'",
+    # )
+    #
+    # # Task to sleep for a few seconds (simulating some work)
+    # sleep_task = BashOperator(
+    #     task_id="sleep_for_5_seconds",
+    #     bash_command="sleep 5",
+    # )
 
     end_task = EmptyOperator(task_id="end")
 
     # Define the task dependencies
-    start_task >> print_message >> sleep_task >> end_task
+    start_task >> print_context() >> end_task
