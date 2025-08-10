3ba1654e71eae8ce4421c254fa3aad9a30c930cc
jonathan5p <jonathanpinzon05@gmail.com>
2025-08-10 15:18:42 +0200

    Changing terraform state container

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -19,7 +19,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "mbeterraformstate"
+    storage_account_name = "mbeterraformstate2"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
