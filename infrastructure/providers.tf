9d04ec6dcc7f88c541fac5fd7b3805fede693554
jonathan5p <jonathanpinzon05@gmail.com>
2025-08-10 15:21:57 +0200

    Fixing resource group

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -18,7 +18,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
+    resource_group_name  = "rg-terraform"
     storage_account_name = "mbeterraformstate2"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
