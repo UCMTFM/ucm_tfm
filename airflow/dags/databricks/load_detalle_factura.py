7b09b8dceeb2010152d64e1373fd41087ac3ec81
jonathan5p <jonathanpinzon05@gmail.com>
2025-08-12 16:01:58 +0200

    Adding sample airflow run job dag

---
+++ airflow/dags/databricks/load_detalle_factura.py
@@ -0,0 +1,17 @@
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
+from commons.enums import DatabricksClusters, AirflowConnections
+
+with DAG(
+    dag_id="load_detalle_factura",
+    start_date=pendulum.now(tz="UTC"),
+    schedule=None,
+    tags=["databricks", "sql", "detalle_factura"],
+) as dag:
+    opr_run_now = DatabricksRunNowOperator(
+        task_id="load_sample_table",
+        databricks_conn_id=AirflowConnections.DATABRICKS_CONN,
+        job_id="1043258284634899",
+    )
