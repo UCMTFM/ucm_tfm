bef7be9162d5a20e5b817bb81e8a0f51e0837743
jonathan5p <jonathanpinzon05@gmail.com>
2025-08-12 11:12:35 +0200

    Addign sparse checkout to the repo

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -194,7 +194,10 @@ module "single_node_compute" {
 resource "databricks_repo" "databricks_notebooks" {
   url    = var.git_repo_https_url
   branch = "main"
-  path   = "databricks_notebooks"
+
+  sparse_checkout {
+    patterns = ["databricks_notebooks/*"]
+  }
 }
 
 # Azure k8s Cluster
