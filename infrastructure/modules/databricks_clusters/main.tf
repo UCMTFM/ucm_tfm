bb8203291da2a6d0097626e1e6ffd0a8bfdb34d9
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-09-07 20:08:06 +0200

    Adding ML to the cluster

--- infrastructure/modules/databricks_clusters/main.tf
+++ infrastructure/modules/databricks_clusters/main.tf
@@ -13,6 +13,7 @@ data "databricks_node_type" "smallest" {
 
 data "databricks_spark_version" "latest_lts" {
   long_term_support = true
+  ml                = true
 }
 
 resource "databricks_cluster" "shared_autoscaling" {
