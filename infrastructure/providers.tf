70e4c331b0d1a5bdedd53f9a48bc45d2dab8baf0
Camilo Cossio <camilocossioalzate2001@gmail.com>
2025-09-07 18:20:31 +0200

    Changing the storage account name?

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -19,7 +19,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-terraform"
-    storage_account_name = "mbeterraformstate2"
+    storage_account_name = "mbeterraformstate1"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
